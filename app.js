090caa65eab909245b62677dfa847cd315db85cf
pradeepshresth <pradeep15shrestha@gmail.com>
2020-05-22 13:57:05 +0545

    first commit

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use((error, req, res, next) => {
 mongoose
     .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-9ufgo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
     .then(() => {
-        app.listen(8080);
+        app.listen(process.env.PORT || 8080);
     })
     .catch(err => {
         console.log(err);
